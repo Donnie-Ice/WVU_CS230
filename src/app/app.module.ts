6d7c8aac1f9ed381a721a5722f416a7e36edda2a
Donnie-Ice <dri0001@mix.wvu.edu>
2021-09-07 11:45:57 -0400

    added body to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BodyComponent } from './body/body.component';
 import { BottomNavBarComponent } from './footer/bottom-nav-bar.component';
 import { TopNavBarComponent } from './header/top-nav-bar.component';
 
@@ -9,7 +10,8 @@ import { TopNavBarComponent } from './header/top-nav-bar.component';
   declarations: [
     AppComponent,
     TopNavBarComponent,
-    BottomNavBarComponent
+    BottomNavBarComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule
