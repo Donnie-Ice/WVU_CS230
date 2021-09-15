162fb4041082798d976efb2c7784d9247a6dba1b
Donnie-Ice <dri0001@mix.wvu.edu>
2021-09-14 21:29:48 -0400

    Seperated top nav bar and body into multiple components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BodyComponent } from './body/body.component';
+import { Body2Component } from './body/body2.component';
+import { Body3Component } from './body/body3.component';
+import { Body4Component } from './body/body4.component';
+import { Body5Component } from './body/body5.component';
 import { BottomNavBarComponent } from './footer/bottom-nav-bar.component';
 import { TopNavBarComponent } from './header/top-nav-bar.component';
+import { TopNavBar2Component } from './header/top-nav-bar2.component';
+import { TopNavBar3Component } from './header/top-nav-bar3.component';
 import { LayoutMainComponent } from './layout/layout-main.component';
 
 @NgModule({
@@ -13,7 +19,13 @@ import { LayoutMainComponent } from './layout/layout-main.component';
     TopNavBarComponent,
     BottomNavBarComponent,
     BodyComponent,
-    LayoutMainComponent
+    LayoutMainComponent,
+    Body2Component,
+    Body3Component,
+    Body4Component,
+    Body5Component,
+    TopNavBar2Component,
+    TopNavBar3Component
   ],
   imports: [
     BrowserModule
