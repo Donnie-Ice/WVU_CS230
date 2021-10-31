641a0867c41bbca09fe55cfc644c68309b1b5d78
Donnie-Ice <dri0001@mix.wvu.edu>
2021-10-31 18:50:01 -0400

    added firebase library

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'nike-app-7911a',
+    appId: '1:276722518870:web:b037813f540fdbad8524f7',
+    databaseURL: 'https://nike-app-7911a-default-rtdb.firebaseio.com',
+    storageBucket: 'nike-app-7911a.appspot.com',
+    apiKey: 'AIzaSyCJk3FMPdnwH6DtmafR1IWSDZcTRiMQO_s',
+    authDomain: 'nike-app-7911a.firebaseapp.com',
+    messagingSenderId: '276722518870',
+    measurementId: 'G-2YJNHXY7E3',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'nike-app-7911a',
+    appId: '1:276722518870:web:b037813f540fdbad8524f7',
+    databaseURL: 'https://nike-app-7911a-default-rtdb.firebaseio.com',
+    storageBucket: 'nike-app-7911a.appspot.com',
+    apiKey: 'AIzaSyCJk3FMPdnwH6DtmafR1IWSDZcTRiMQO_s',
+    authDomain: 'nike-app-7911a.firebaseapp.com',
+    messagingSenderId: '276722518870',
+    measurementId: 'G-2YJNHXY7E3',
+  },
   production: false
 };
 
